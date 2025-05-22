1449d4183e670fd1bcd19e695230cae0454e3f14
ManjeetBaliyan <manjeetbaliyan21@gmail.com>
2025-05-22 10:20:44 +0530

    Initial Commit

---
+++ admin/src/App.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import Login from './pages/login'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
+import { useContext } from 'react';
+import { AdminContext } from './context/AdminContext';
+import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
+import { Route, Routes } from 'react-router-dom'
+import AddDoctor from './pages/Admin/AddDoctor'
+import AllApointments from './pages/Admin/AllApointments'
+import Dashboard from './pages/Admin/Dashboard'
+import DoctorsList from './pages/Admin/DoctorList'
+import { DoctorContext } from './context/DoctorContext';
+import DoctorDashboard from './pages/Doctor/DoctorDashboard';
+import DoctorAppointment from './pages/Doctor/DoctorAppointment';
+import DoctorProfile from './pages/Doctor/DoctorProfile';
+
+
+
+function App() {
+
+  const { aToken } = useContext(AdminContext)
+  const { dToken } = useContext(DoctorContext)
+
+  //Here we add the logic so that if we have the admin token in that case we will hide login page and we will display rest of the pages for the admin.
+  return aToken || dToken ? (
+
+    <div className='bg-[#F8F9FD]'>
+
+      <ToastContainer />
+      <Navbar />
+
+      <div className='flex items-start'>
+        <Sidebar />
+
+        <Routes>
+          {/* ---ADMIN ROUTES----- */}
+          <Route path='/' element={<></>} />
+          <Route path='/add-doctor' element={<AddDoctor />} />
+          <Route path='/all-appointments' element={<AllApointments />} />
+          <Route path='/admin-dashboard' element={<Dashboard />} />
+          <Route path='/doctor-list' element={<DoctorsList />} />
+
+          {/*--------DOCTOR ROUTES------------*/}
+          <Route path='/doctor-dashboard' element={<DoctorDashboard />} />
+          <Route path='/doctor-appointments' element={<DoctorAppointment />} />
+          <Route path='/doctor-profile' element={<DoctorProfile />} />
+
+        </Routes>
+      </div>
+
+    </div>
+  ) : (
+    <>
+      <Login />
+      <ToastContainer />
+    </>
+  )
+}
+
+export default App
---
+++ admin/tailwind.config.js
@@ -0,0 +1,14 @@
+ /** @type {import('tailwindcss').Config} */
+export default {
+   content: [
+    "./index.html",
+    "./src/**/*.{html,js,jsx}"],
+   theme: {
+     extend: {
+      colors:{
+        'primary':"#5F6FFF"
+      }
+     },
+   },
+   plugins: [],
+ }
