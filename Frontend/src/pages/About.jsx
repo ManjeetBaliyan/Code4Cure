75f0977359a6da31ca4448b6d11375bc5d3d749b
Vansh Tomar <vanshtomar2906@gmail.com>
2025-05-22 15:57:51 +0530

    message

--- Frontend/src/pages/About.jsx
+++ Frontend/src/pages/About.jsx
@@ -12,10 +12,10 @@ function About() {
       <div className='my-10 flex flex-col md:flex-row gap-12'>
         <img className='w-full md:max-w-[360px]' src={assets.about_image} alt="" />
         <div className='flex flex-col justify-center gap-6 md:w-2/4 text-sm text-gray-600'>
-          <p>Welcome to Prescripto, your trusted partner in managing your healthcare needs conveniently and efficiently. At Prescripto, we understand the challenges individuals face when it comes to scheduling doctor appointments and managing their health records.</p>
-          <p>Prescripto is committed to excellence in healthcare technology. We continuously strive to enhance our platform, integrating the latest advancements to improve user experience and deliver superior service. Whether you're booking your first appointment or managing ongoing care, Prescripto is here to support you every step of the way.</p>
+          <p>Welcome to MyDoctor, your reliable companion in navigating modern healthcare with ease and confidence. At MyDoctor, we recognize the daily challenges people face in accessing timely medical care and managing their health journey.</p>
+          <p>We are dedicated to simplifying healthcare through smart technology—allowing you to book appointments, access trusted doctors, and manage medical interactions all in one place. Whether you're seeking a one-time consultation or managing regular check-ups, MyDoctor is designed to support you every step of the way.</p>
           <b className='text-gray-800'>Our Vision</b>
-          <p>Our vision at Prescripto is to create a seamless healthcare experience for every user. We aim to bridge the gap between patients and healthcare providers, making it easier for you to access the care you need, when you need it.</p>
+          <p>At MyDoctor, our vision is to revolutionize the way people experience healthcare. We strive to build a connected, accessible, and patient-centered healthcare ecosystem where care is just a click away—anytime, anywhere.</p>
         </div>
       </div>
 
